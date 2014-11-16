2d44b362bf6ceecc19f9763bbae2207e07ac552f
Ismail Hassan <ismail.bukittinggi@gmail.com>
2014-11-17 06:13:24 +0700

    Added Start Finish area trigger. Added Minimap. Create prefab objects.

---
+++ Assets/CKP-BGA/Scripts/MovementController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+
+    public Transform startArea;
+    private bool win;
+	// Use this for initialization
+	void Start () {
+        win = false;
+        this.transform.position = startArea.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach (Transform child in transform) {
+            if (child.name.Contains("Wheel")) {
+                child.Rotate(new Vector3(0,-1,0) * Time.deltaTime * 90);
+                
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider other) {
+        win = true;
+    }
+
+    void OnGUI() {
+        if (win) {
+            GUI.TextArea (new Rect (0, 200, 100, 50), "WIN!!");
+        }
+    }
+
+}
