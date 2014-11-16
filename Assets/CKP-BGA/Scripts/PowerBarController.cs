b28d55ac2efd4d9f1e6c314bbd41b0ca4dc4bd1c
seiryuu <ed.elric643@gmail.com>
2014-11-17 01:58:45 +0700

    PowerBar script added

---
+++ Assets/CKP-BGA/Scripts/PowerBarController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerBarController : MonoBehaviour {
+
+    public GameObject player;
+    public Texture2D image;
+
+    void OnGUI()
+    {
+        GUI.DrawTextureWithTexCoords (new Rect (5, 5, player.power, 20), image, new Rect (0, 0, (float)(player.power / image.width), 1));
+    }
+}
