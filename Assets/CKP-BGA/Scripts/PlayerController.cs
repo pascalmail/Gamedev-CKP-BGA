94643a5a7b519f5333854674ef2856e4bad66d35
Ismail Hassan <ismail.bukittinggi@gmail.com>
2014-11-17 03:49:59 +0700

    Organize Asset folder.

--- Assets/CKP-BGA/Scripts/PlayerController.cs
+++ Assets/CKP-BGA/Scripts/PlayerController.cs
@@ -19,35 +19,35 @@ public class PlayerController : MonoBehaviour
 
     void Update ()
     {
-        
-    }
-
-
-    void FixedUpdate ()
-    {
         if (!isMoving) {
             if (Input.GetButtonDown ("Jump")) {
                 isCharging = true;
             }
             if (Input.GetButtonUp ("Jump")) {
                 isCharging = false;
-                Vector3 impulse = this.transform.localToWorldMatrix.MultiplyVector(new Vector3(0,0,this.rigidbody.mass*power));
-                this.rigidbody.AddForce(impulse, ForceMode.Impulse);
-//                this.rigidbody.velocity = this.transform.localToWorldMatrix * new Vector4(0, 0, speed, 0);
+                Vector3 impulse = this.transform.localToWorldMatrix.MultiplyVector (new Vector3 (0, 0, this.rigidbody.mass * power));
+                this.rigidbody.AddForce (impulse, ForceMode.Impulse);
             }
-
-            float rotateLeft = Input.GetAxis ("Horizontal");
-            this.transform.Rotate (new Vector3 (0, 1, 0), rotateLeft * 180 * Time.deltaTime);
         }
         if (isCharging) {
             power += MAX_POWER * Time.deltaTime;
             if (power > MAX_POWER) {
                 power = MAX_POWER;
-
+                
             }
         } else {
             power = 0;
         }
+    }
+
+
+    void FixedUpdate ()
+    {
+        if (!isMoving) {
+            float rotateLeft = Input.GetAxis ("Horizontal");
+            this.transform.Rotate (new Vector3 (0, 1, 0), rotateLeft * 180 * Time.deltaTime);
+        }
+        
     }
 
     void OnCollisionEnter(Collision other) {
--- Assets/CKP-BGA/Scripts/PowerBarController.cs
+++ Assets/CKP-BGA/Scripts/PowerBarController.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class PowerBarController : MonoBehaviour {
 
-    public GameObject player;
+    public PlayerController pController;
     public Texture2D image;
+    public Texture2D emptyBarTexture;
 
     void OnGUI()
-    {
-        GUI.DrawTextureWithTexCoords (new Rect (5, 5, player.power, 20), image, new Rect (0, 0, (float)(player.power / image.width), 1));
+    {   
+        GUI.Box (new Rect (0, 0, image.width, image.height), "POWER");
+        float filledWidth = pController.power / PlayerController.MAX_POWER * image.width;
+        GUI.DrawTextureWithTexCoords (new Rect (5, 5, filledWidth, 20), image, new Rect (0, 0, (float)(filledWidth / image.width), 1));
     }
 }
