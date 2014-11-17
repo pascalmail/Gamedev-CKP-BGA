1afced760b6487b504b8ae26b709292f6c23a908
Ismail Hassan <ismail.bukittinggi@gmail.com>
2014-11-17 11:43:32 +0700

    Added Toon Shading. Added simple challange for tester level.

---
+++ Assets/MiniMapController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMapController : MonoBehaviour {
+
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        //this.camera.pixelRect = new Rect (0, 0, 100, 100);
+        this.camera.fieldOfView = 45;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
