654b65fae74dd412a0eea5f86c65ea7b7b61d371
Ismail Hassan <ismail.bukittinggi@gmail.com>
2014-12-07 20:03:27 +0700

    Added skeleton GameController

--- Assets/CKP-BGA/Scripts/GameController.cs
+++ Assets/CKP-BGA/Scripts/GameController.cs
@@ -1,12 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour
 {
-    public void Start() {
+    public int turnNumber;
+    public int actorPlaying;
+    private GameObject[] actor;
+    private PlayerController[] states;
+
+    void Start() {
+        actor = GameObject.FindGameObjectsWithTag("Player");
+        states = GameObject.FindObjectsOfType<PlayerController> ();
+//        actor[0].GetC
+
     }
 
-    public void Update() {
 
+
+    void Update() {
+
+        // find angle
+
+        // find power
+
+        // shoot
+
+        // save status
+
+        // change
     }
+
+
+    IEnumerable<int> test() {
+        for(int i = 0; i < 5; ++i ) {
+            yield return i;
+        }
+    }
+
 }
